12d523ea9d5ed6a784b2d6a7a203101f226cf196
Sky Lin <sky798lin@gmail.com>
2024-01-24 17:53:11 -0500

    Initial commit

---
+++ src/main/java/sky798lin/MyWorkoutTracker/MyWorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package sky798lin.MyWorkoutTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkoutTrackerApplication.class, args);
+	}
+
+}
